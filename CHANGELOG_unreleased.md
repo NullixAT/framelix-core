0445bf66ab6e51e48b8fd259ab57f027169e8536
BrainFooLong <brainfoolong@users.noreply.github.com>
2022-05-04 11:16:38 +0200

    added Tar class to create and extract tar files

--- CHANGELOG_unreleased.md
+++ CHANGELOG_unreleased.md
@@ -16,6 +16,7 @@
 * added noAnimation option to FramelixModal
 * added QuickSearch->forceInitialQuery to set a initial query no matter what the user have stored
 * added fieldGroups to Forms, to be able to group fields under a collapsable
+* added `Tar` class to create and extract tar files
 * refactored language handling for more flexible way to load and add values
 * refactored update and release process
 * removed a few unused user roles
---
+++ src/Utils/Tar.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Framelix\Framelix\Utils;
+
+use Framelix\Framelix\ErrorCode;
+use Framelix\Framelix\Exception;
+use Phar;
+use PharData;
+use function file_exists;
+use function is_dir;
+use function is_file;
+use function scandir;
+
+/**
+ * Tar utilities
+ */
+class Tar
+{
+    /**
+     * Create a tarball file
+     * @param string $tarPath
+     * @param string[] $files Key is path in tar, value is full filepath on disk
+     */
+    public static function createTar(
+        string $tarPath,
+        array $files
+    ): void {
+        $archive = new PharData($tarPath);
+        $dirsCreated = [];
+        foreach ($files as $relativeName => $fullPath) {
+            $fullPath = FileUtils::normalizePath($fullPath);
+            if (is_dir($fullPath)) {
+                if (!isset($dirsCreated[$fullPath])) {
+                    $dirsCreated[$fullPath] = true;
+                    $archive->addEmptyDir($relativeName);
+                }
+            } elseif (file_exists($fullPath)) {
+                $archive->addFile($fullPath, $relativeName);
+            }
+        }
+    }
+
+    /**
+     * Extract a tarball's files into given directory
+     * @param string $tarPath
+     * @param string $outputDirectory
+     * @param bool $skipNotEmptyDirectory If true, it unpacks even if directory is not empty
+     * @return void
+     */
+    public static function extractTo(
+        string $tarPath,
+        string $outputDirectory,
+        bool $skipNotEmptyDirectory = false
+    ): void {
+        if (!is_file($tarPath)) {
+            throw new Exception("'$tarPath' is no file", ErrorCode::TAR_EXTRACT_NOFILE);
+        }
+        if (!is_dir($outputDirectory)) {
+            throw new Exception("'$outputDirectory' is no directory", ErrorCode::TAR_EXTRACT_NODIRECTORY);
+        }
+        $archive = new PharData($tarPath);
+        if (!$skipNotEmptyDirectory) {
+            $files = scandir($outputDirectory);
+            if (count($files) > 2) {
+                throw new Exception("'$outputDirectory' is not empty", ErrorCode::TAR_EXTRACT_NOTEMPTY);
+            }
+        }
+        $archive->extractTo($outputDirectory, overwrite: true);
+    }
+}
